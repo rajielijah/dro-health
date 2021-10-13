5bde3ee1ebc6e70db174407ebcee03f1b64228eb
Raji Elijah <rajielijah@gmail.com>
2021-10-13 23:29:28 +0100

    dev

--- lib/screens/pharmacy_view.dart
+++ lib/screens/pharmacy_view.dart
@@ -1,4 +1,5 @@
 import 'package:drohealth/constant/colors.dart';
+import 'package:drohealth/widget/categoris_widgte.dart';
 import 'package:flutter/material.dart';
 
 class Pharmacy extends StatelessWidget {
@@ -17,6 +18,7 @@ class Pharmacy extends StatelessWidget {
                 height: 150,
                 width: width,
                 decoration: BoxDecoration(
+                  color: Colors.grey[400],
                   borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(20),
                       bottomRight: Radius.circular(20)),
@@ -76,7 +78,139 @@ class Pharmacy extends StatelessWidget {
                         fontWeight: FontWeight.w600),
                   )
                 ],
-              )
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: height,
+                color: Colors.white70,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 18.0, right: 18),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "CATEGORIES",
+                            style: TextStyle(
+                                color: Colors.grey[400],
+                                fontSize: 18,
+                                fontFamily: "Proxima Nova",
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            "VIEW ALL",
+                            style: TextStyle(
+                                color: DROMiddleBlue,
+                                fontFamily: "Proxima Nova",
+                                fontWeight: FontWeight.w500),
+                          )
+                        ],
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          categoriesWidget(
+                              context,
+                              Image.asset("assets/images/category.png"),
+                              "Headache"),
+                          categoriesWidget(
+                              context,
+                              Image.asset("assets/images/category4.png"),
+                              "Supplements"),
+                          categoriesWidget(context,
+                              Image.asset("assets/images/cate.png"), "Infacts"),
+                          categoriesWidget(
+                              context,
+                              Image.asset("assets/images/category2.png"),
+                              "Cough")
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 18.0, right: 18),
+                      child: Text(
+                        "SUGGESTIONS",
+                        style: TextStyle(
+                            color: Colors.grey[400],
+                            fontSize: 18,
+                            fontFamily: "Proxima Nova",
+                            fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Container(
+                        height: 215,
+                        width: 168,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[100],
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(0.0, 1.0), //(x,y)
+                              blurRadius: 16.0,
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              "assets/images/sup1.png",
+                              height: 127,
+                              width: 168,
+                              fit: BoxFit.fitWidth,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Paracetamol",
+                                style: TextStyle(
+                                    // color: Colors.white,
+                                    fontFamily: "Proxima Nova",
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(right: 8.0, left: 8),
+                              child: Text(
+                                "Tablet * 500mg",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: "Proxima Nova",
+                                    fontSize: 15,
+                                    color: Colors.black45),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "\$350.00",
+                                style: TextStyle(
+                                    fontFamily: "Proxima Nova",
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w800),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
--- lib/widget/categoris_widgte.dart
+++ lib/widget/categoris_widgte.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
-Widget categories(BuildContext context, Image image, Text text) {
-  return Container();
+Widget categoriesWidget(BuildContext context, Image image, String text) {
+  return Container(
+    height: 90,
+    width: 130,
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+    child: Stack(
+      children: [
+        image,
+        Positioned(
+          top: 37,
+          left: 40,
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white, fontFamily: "Proxima Nova"),
+          ),
+        )
+      ],
+    ),
+  );
 }
